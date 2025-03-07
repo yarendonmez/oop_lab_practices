bc7125eac3d7646ae71fcf3688107166b954520e
yarendonmez <yarendonmez034@gmail.com>
2025-03-07 23:42:45 +0300

    Employee.java

---
+++ src/main/java/com/yarendonmez/week1/Employee.java
@@ -0,0 +1,27 @@
+package com.yarendonmez.week1;
+
+public class Employee {String name;
+    String position;
+    double salary;
+    int yearsWorked;
+
+    Employee(String name, String position, double salary, int yearsWorked){
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.yearsWorked = yearsWorked;
+    }
+
+    void showInfo(){
+        System.out.println("Çalışan bilgileri:");
+        System.out.println("Adı: "+name);
+        System.out.println("Pozisyon: "+position);
+        System.out.println("Maaş: "+salary);
+        System.out.println("Çalışma süresi: "+yearsWorked+" yıl.");
+    }
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Zeynep","En iyi asistan",100000,5);
+        employee1.showInfo();
+    }
+}
