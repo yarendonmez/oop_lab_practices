bad867c0eca758823ef72f400baefd4eebe06996
Yaren Dönmez <yarendonmez034@gmail.com>
2025-04-13 18:18:06 +0300

    MusicianNewVersion

---
+++ src/main/java/com/yarendonmez/midtermPractice/UML2/Car.java
@@ -0,0 +1,29 @@
+package com.yarendonmez.midtermPractice.UML2;
+
+import java.sql.SQLOutput;
+
+public class Car {
+    private String carColor;
+    private double carPrice;
+
+    //getter carColor
+    public String getCarColor(String model){
+        System.out.println(model + "için arama yapılıyor...");
+        return carColor;
+    }
+
+    //setter carColor
+    public String setCarColor(String carColor){
+        this.carColor = carColor;
+    }
+
+}
+class CarTest{
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.setCarColor("Black");
+        String myCarColor = myCar.getCarColor("BMW");
+
+        System.out.println(myCarColor);
+    }
+}
