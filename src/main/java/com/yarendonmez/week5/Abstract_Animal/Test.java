66c3929dfdad2fad65e31e4e8e2c74ceb0816a15
Yaren Dönmez <yarendonmez034@gmail.com>
2025-04-24 16:47:44 +0300

    Polymorphism güncelleme

--- src/main/java/com/yarendonmez/week5/Abstract_Animal/Test.java
+++ src/main/java/com/yarendonmez/week5/Abstract_Animal/Test.java
@@ -3,7 +3,9 @@ package com.yarendonmez.week5.Abstract_Animal;
 public class Test {
     public static void main(String[] args) {
         // Cat nesnesi oluşturuluyor
-        Cat myCat = new Cat("Mia");
+        //Cat myCat = new Cat("Mia");
+        //görgü kuralı polymorphism
+        Animal myCat = new Cat("Mia");
 
         // Metotlar test ediliyor
         myCat.makeSound();  // Çıktı: Meow
