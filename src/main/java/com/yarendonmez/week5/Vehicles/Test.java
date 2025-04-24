2899d414c8c2f9234d0547ead89a6a1b4faa3a51
Yaren Dönmez <yarendonmez034@gmail.com>
2025-04-24 16:50:49 +0300

    Polymorphism güncelleme

--- src/main/java/com/yarendonmez/week5/Vehicles/Test.java
+++ src/main/java/com/yarendonmez/week5/Vehicles/Test.java
@@ -4,19 +4,20 @@ public class Test {
     public static void main(String[] args) {
 
         //Create objects
-        Car myCar = new Car();
-        Bicycle myBicycle = new Bicycle();
+        Vehicle myCar = new Car();
+        Vehicle myBicycle = new Bicycle();
 
         // Call methots
         myCar.ride();
         myCar.load();
         myCar.load(50);
+
         System.out.println();
 
         myBicycle.ride();
         myBicycle.load();
         myBicycle.load(3);
-        System.out.println();
+
 
     }
 
